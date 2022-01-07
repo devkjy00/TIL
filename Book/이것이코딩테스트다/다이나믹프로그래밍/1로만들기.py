2100ecbadfa7d036e98c7f3e51507a0c4297a92d
김주영 <anwjsrlrhwkd@naver.com>
2022-01-07 17:43:49 +0900

    .

---
+++ Book/이것이코딩테스트다/다이나믹프로그래밍/1로만들기.py
@@ -0,0 +1,22 @@
+# 주어진 수 X를 5,3,2로 나누거나 1을 빼서 1로 만드는 최소횟수
+
+x = 72
+
+d = [0] * 30001
+
+
+for i in range(2, x+1):
+    d[i] = d[i-1] + 1
+
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2]+1)
+    
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3]+1) 
+
+    if i % 5 == 0:
+        d[i] = min(d[i], d[i // 5]+1)
+    print(d[i])
+
+
+print("count:",d[x])
