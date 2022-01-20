4f8440171c455036a8daa14291f6df283a70f671
southoftheriver <anwjsrlrhwkd@naver.com>
2022-01-20 15:45:57 +0000

    .

--- 코딩테스트/백준/셀프넘버.py
+++ 코딩테스트/백준/셀프넘버.py
@@ -3,18 +3,18 @@
 # 생성자가 없는 수는 셀프넘버라고 한다
 # 10000까지의셀프넘버 구하기
 
-def d(num: int)->int:
-    result = num
-    x = 0
-
-    len_num = len(str(num))
-
-    for _ in range(len_num):
-        x = num%10
-        num //= 10
-        result += x
-
-    return result
+#def d(num: int)->int:
+#    result = num
+#    x = 0
+#
+#    len_num = len(str(num))
+#
+#    for _ in range(len_num):
+#        x = num%10
+#        num //= 10
+#        result += x
+#
+#    return result
 
 n = 10000
 
@@ -22,11 +22,17 @@ self_num = [True] * (n+1)
 self_num[0] = False
 
 for i in range(1, n):
-    if (idx := d(i)) > n:
-        break
-    self_num[idx] = False
+#    if (idx := d(i)) > n:
+#        break
+#    self_num[idx] = False
+    # 문자열로 변환, for문에 돌려서 간단하게할 수 있다
+    x = 0
+    for j in str(i):
+        x += int(j)
+    if i+x > n:
+        continue
+    self_num[i+x] = False
 
 for idx, b in enumerate(self_num):
     if b:
         print(idx)
-
