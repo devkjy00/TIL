5e58569f613f40c4506991b02c0777a13087d007
southoftheriver <anwjsrlrhwkd@naver.com>
2022-02-07 19:21:52 +0900

    객체지향

--- Book/자바의정석/연습문제/Pr_15.java
+++ Book/자바의정석/연습문제/Pr_15.java
@@ -6,8 +6,12 @@
 // 5. 파일에서 태그만 빼고 출력하기
 import java.io.*;
 import java.util.*;
+
+import javax.lang.model.util.AbstractAnnotationValueVisitor6;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.beans.EventHandler;
 
 public class Pr_15 {
     public static void main(String[] args) {
@@ -152,13 +156,7 @@ public class Pr_15 {
         //         System.out.println("입력오류입니다.");
         //     }
         // } //while(true)
-        
-        if(args.length != 1) {
-            System.out.println("USAGE : java ChatWin NICKNAME");
-            System.exit(0);
-        }
-        new ChatWin(args[0]);
-        
+
     } // main
 
     static int totalFiles = 0;
@@ -254,89 +252,3 @@ class HtmlTagFilterWriter extends FilterWriter {
     }
 } // class HtmlTagFilterWriter 
 
-
-class ChatWin extends Frame {
-    String nickname = "";
-    TextArea ta = new TextArea();
-    Panel p = new Panel();
-    TextField tf = new TextField(30);
-    Button bSave = new Button("저장");
-
-    ChatWin(String nickname) {
-        super("Chatting");
-        this.nickname = nickname;
-
-        setBounds(200, 100, 300, 200);
-
-        p.setLayout(new FlowLayout());
-        p.add(tf);
-        p.add(bSave);
-
-        add(ta, "Center");
-        add(p, "South");
-        
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-
-        bSave.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent ae) {
-                FileDialog fileSave = new FileDialog(ChatWin.this, 
-                                "파일저장", FileDialog.SAVE);
-                
-                fileSave.setVisible(true);
-                String fileName = fileSave.getDirectory() + fileSave.getFile();
-                saveAs(fileName);
-            }
-        });
-
-        EventHandler  handler = new EventHandler();
-        ta.addFocusListener(handler);
-        tf.addFocusListener(handler);
-        tf.addActionListener(handler);
-
-        ta.setText("#" + nickname + "님 즐거운 채팅 되세요.");
-        ta.setEditable(false);
-
-        setResizable(false);
-        setVisible(true);
-        tf.requestFocus();
-    }
-
-    
-} // class ChatWin
-
-void saveAs(String fileName) {
-
-    FileWriter fw = null;
-    BufferedWriter bw = null;
-
-    try {
-        fw = new FileWriter(fileName);
-        bw = new BufferedWriter(fw);
-        bw.write(ta.getText());
-
-    } catch (IOException ie) {
-        ie.printStackTrace();
-    } finally {
-        try {
-            if (bw != null)
-                bw.close();
-        } catch (IOException e) { }
-    }
-}
-
-class EventHandler extends FocusAdapter implements ActionListener {
-    public void actionPerformed(ActionEvent ae) {
-        String msg = tf.getText();
-        if("".equals(msg)) return;
-
-        ta.append("\r\n" + nickname + ">" + msg);
-    }
-
-    public void focusGained(FocusEvent e) {
-        tf.requestFocus();
-    }
-} // EventHandler 
