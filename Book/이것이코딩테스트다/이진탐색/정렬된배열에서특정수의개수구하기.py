1fa5747b7f1f72a496cde42f0ba4429c8e08e54e
southoftheriver <anwjsrlrhwkd@naver.com>
2022-03-07 20:14:23 +0900

    til

--- Book/이것이코딩테스트다/이진탐색/정렬된배열에서특정수의개수구하기.py
+++ Book/이것이코딩테스트다/이진탐색/정렬된배열에서특정수의개수구하기.py
@@ -3,7 +3,7 @@ N개의 오름차순 정렬된 원소
 x가 등장하는 횟수를 O(logN)시간복잡도로 구하라
 """
 
-from bisect import bisect_left, bisect_right 
+from bisect import bisect_left, bisect_right
 
 
 _, x = 7, 2
@@ -11,21 +11,11 @@ nums = [1, 1, 2, 2, 2, 2, 3]
 
 
 def mysolution(x: int, nums: list[int]) -> int:
-    a = bisect_right(nums, x)
-    b = bisect_left(nums, x)
-    return a-b
+    return bisect_right(nums, x)-bisect_left(nums, x)
 
 
-def 이진검색직접구현(x, nums):
-    n = len(nums)
-    if(a := first(nums, x, 0 , n-1)==None):
-        return 0
-    # b = last(nums, x, 0, n-1)
-    # return b - a + 1
-
-def first(array, target, start, end):
-    pass
-    
-    
 result = mysolution(x, nums)
-print(result)
+if result == 0:
+    print(-1)
+else:
+    print(result)
