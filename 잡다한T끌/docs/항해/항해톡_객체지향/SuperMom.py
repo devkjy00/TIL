b0c38f4bc9d0d94a14861d7804dac4cc909ea9df
southoftheriver <anwjsrlrhwkd@naver.com>
2022-05-22 23:07:11 +0900

    til

---
+++ 잡다한T끌/docs/항해/항해톡_객체지향/SuperMom.py
@@ -0,0 +1,15 @@
+'''
+class SuperMom
+    -> commute() : 직장으로 출근한다
+    -> get_children_to_school() : 자녀들을 어린이집에 등원시킨다
+'''
+
+class SuperMom:
+    def __init__(self):
+        self.time = 8
+
+    def commute(self):
+
+
+    def get_children_to_school(self):
+
