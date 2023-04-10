e214398199b7817f6a1a87a8aa991a6b5bab23ff
southoftheriver <anwjsrlrhwkd@naver.com>
2023-04-10 17:40:30 +0900

    강의 리액트

---
+++ Lecture/처음만난리액트_inf/project/test/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card"
+import React from "react";
+
+function ProfileCard(props) {
+    return (
+        <Card title="Inje Lee" backgroundColor="#4ea04e">
+            <p>안녕하세요</p>
+            <p>개발 중 입니다</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
